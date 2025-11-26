f369ac3740d4db716e3d975ca5df54e6f4279c4f
savidi <savindyawick@gmail.com>
2025-11-26 22:11:24 +0530

    cardio to report

--- echo-dashboard/vite.config.js
+++ echo-dashboard/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 // The repository name, including leading and trailing slashes
-const REPO_NAME = '/nhsl-echo-cardio/' 
+const REPO_NAME = '/nhsl-echo-report/' 
 // https://vite.dev/config/
 export default defineConfig({
   base: REPO_NAME, 
